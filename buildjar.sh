1e80c08f662790c1ed34fa73b74ee07aa0414001
Michael Henke <smack42@gmail.com>
2022-09-04 21:29:44 +0200

    buildjar.sh compiles using javac

--- buildjar.sh
+++ buildjar.sh
@@ -1,5 +1,16 @@
 JARFILE=infect.jar
+BINDIR=bin
+SRCDIR=src
 
+echo compile...
+mkdir -p $BINDIR
+javac -d $BINDIR -source 1.8 -target 1.8 -Xlint $SRCDIR/infect/g2Ddemo/*.java
+
+echo build jar...
 rm -f $JARFILE
 jar -c -e infect.g2Ddemo.Launcher -f $JARFILE -C bin/ .
 chmod u+x $JARFILE
+
+echo done.
+echo to run it, use:  java -jar $JARFILE
+
